219c15415f7786bce07eaa232304bfb9e908083b
Katie Starrett <starrett.k.e@gmail.com>
2017-05-18 16:55:47 -0400

    add initial var and functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,21 @@ var wins;
 var losses;
 var guessesRemaining;
 var words = ["chickadee", "cardinal", "mourning dove", "blue jay", "house sparrow", "starling", "grackle", "woodpecker", "nuthatch", "tufted titmouse", "wren", "robin", "goldfinch", "house finch"];
-var currentWord = words[Math.floor(Math.random() * words.length)];
+var currentWord;
+var lettersToGuess = [];//or string, idk
+var visibleWord = [];//or string
+var lettersGuessed = "";
 
-var answerList = (currentWord.split(""));
-
+function pickWord() {
+	currentWord = words[Math.floor(Math.random() * words.length)];
+};
+function reset() {
+	pickWord();
+	guessesRemaining = currentWord.length + 5;
+	document.getElementById("numGuess").innerHTML = guessesRemaining;
+	lettersGuessed = "";
+	document.getElementById("lettersGuess").innerHTML = lettersGuessed;
+	lettersToGuess = currentWord.split("");
+	visibleWord = lettersToGuess.replace("&nbsp_&nbsp");
+	document.getElementById("word").innerHTML = visibleWord;
+}
